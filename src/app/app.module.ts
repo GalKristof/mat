8f31254b88ee4d3d895eff16f7839e99819c07c9
Gál Kristóf <kristof.gal@nixs.com>
2024-10-29 22:39:04 +0100

    Create first components, make basic changes, install first nodes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { MenuComponent } from './menu/menu.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    MenuComponent,
+    HeaderComponent,
+    FooterComponent,
+    TestimonialComponent
   ],
   imports: [
     BrowserModule,
